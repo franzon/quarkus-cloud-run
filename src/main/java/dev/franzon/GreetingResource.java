fb7b68a15e43ba9cb957845b94e5c467bc5bfcf8
Jorge Rossi <jorgefzrossi@gmail.com>
2021-10-03 17:45:03 -0300

    feat: version 3

--- src/main/java/dev/franzon/GreetingResource.java
+++ src/main/java/dev/franzon/GreetingResource.java
@@ -11,6 +11,6 @@ public class GreetingResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello RESTEasy version 2";
+        return "Hello RESTEasy version 3";
     }
 }
